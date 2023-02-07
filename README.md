6c9af549c84fb73fa1305b8241115df9736e0cc0
Banyaphon Ramsiri <hzwtester001@gmail.com>
2023-02-07 15:46:12 +0700

    update version tf2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# regression_food_train
+# Estimation
+```python calorie_estimation.py --path path --option train/test```
+- In that path must contains 3 directories (csv/test/train)
+- In csv directory contains test/train.csv and csv header is ```filename, weight, food```
+- In train/test directories contain image of divided food tray
+#Example command
+- python calorie_estimation.py --option train --path ./dataset/regression
---
+++ calorie_estimation.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import os,shutil,math,scipy,cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rn
+import sys
+import argparse
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from tqdm import tqdm
+
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
+from keras import optimizers
+from keras.models import Sequential, Model, load_model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.callbacks import ModelCheckpoint,EarlyStopping,TensorBoard,CSVLogger,ReduceLROnPlateau,LearningRateScheduler
+from keras import backend as K
+
+def weight_mapping(filename, label):
+        return label[label['filename']==filename]['weight'].values[0]
+
+def means(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return mean_squared_error(y_true,y_pred)
+    
+def mean_absolute_percentage_error(y_true, y_pred): 
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser_options = ['--path', '--option']
+
+    for parser_option in parser_options:
+        parser.add_argument(parser_option)
+
+    args = parser.parse_args()
+
+    imgsize = 200
+    x = []
+    y = []
+    food = []
+    label = .(os.path.join(args.path, 'csv', args.option+'.csv'))
+    path_data = os.path.join(args.path, args.option)
+
+    for file in tqdm(os.listdir(path_data)):
+        weight = weight_mapping(file, label)
+        img = cv2.imread(os.path.join(path_data, file), cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (imgsize, imgsize))
+        x.append(np.array(img))
+        y.append(weight)
+        food.append(label[label['filename']==file]['food'].values[0])
+
+    x = np.array(x)
+    y = np.array(y)
+    
+    print(str(food))
+    print(len(food))
+    #print(str(y))
+    
+    print(args.option)
+    if args.option=='train':
+        base_model = InceptionResNetV2(include_top=False,
+                    input_shape = (imgsize,imgsize,3),
+                    weights = 'imagenet')
+        model = Sequential()
+        model.add(base_model)
+        model.add(GlobalAveragePooling2D())
+        model.add(Dropout(0.25))
+        model.add(Dense(1, activation='relu'))
+        model.summary()
+
+        model.compile(
+            loss='mean_squared_error',
+            optimizer='adam',
+        )
+        augs_gen = ImageDataGenerator(
+            rotation_range=180,  
+            horizontal_flip=True,  
+            vertical_flip=True,
+            dtype='uint8'
+        )
+        history = model.fit_generator(
+            augs_gen.flow(x, y, batch_size=10),
+            steps_per_epoch  = 200,
+            epochs = 100, 
+            verbose = 1,
+        )
+        model.save("model_estimation.h5")
+    elif args.option=='evo':
+                model = load_model('modelestimation.h5',compile=False)
+                model.get_weights()
+                y_pred = model.predict(x)
+                result = pd.DataFrame({'real':y.flatten(), 'predict':y_pred.flatten()})
+                result['food'] = food
+                for i in result['food'].unique():
+                    print(result[i])
+                #print(model.get_weights())
+                """model = load_model('modelestimation.h5',compile=False)
+                #model.layers[dense_1].output.get_shape()
+                inp = model.input
+                out = [layer.output for layer in model.layers]
+                get_outputs = K.function([inp, K.learning_phase()], out)
+                print(get_outputs)"""
+
+
+
+     
+    elif args.option=='test_9':
+        model = load_model('modelestimation.h5',compile=False)
+        y_pred = model.predict(x)
+
+        result = pd.DataFrame({'real':y.flatten(), 'predict':y_pred.flatten()})
+        result['food'] = food
+
+        report = dict()
+        report['id'] = list(result['food'].unique())
+        report['mape'] = []
+        report['mse'] = []
+        print('Mean absolute percentage error')
+        print('Mean squared error')
+        for i in result['food'].unique():
+            food = result[result['food']==i]
+            mape = mean_absolute_percentage_error(food['real'],food['predict'])
+            report['mape'].append(mape)
+            print('mape: '+i+': '+str(mape))
+        for i in result['food'].unique():
+            food = result[result['food']==i]
+            mse = means(food['real'],food['predict'])
+            report['mse'].append(mse)
+            print('mse: '+i+': '+str(mse))
+
+
+    elif args.option=='test_mse':
+        model = load_model('modelestimation.h5',compile=False)
+        y_pred = model.predict(x)
+
+        result = pd.DataFrame({'real':y.flatten(), 'predict':y_pred.flatten()})
+        result['food'] = food
+
+        report = dict()
+        report['id'] = list(result['food'].unique())
+        report['mse'] = []
+        print('Mean squared error')
+        for i in result['food'].unique():
+            food = result[result['food']==i]
+            mse = means(food['real'],food['predict'])
+            report['mse'].append(mse)
+            print(i+': '+str(mse))
+
